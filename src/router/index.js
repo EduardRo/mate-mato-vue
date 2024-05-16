2392401b0a5edc84a43dd77416848bc25c3a129a
EduardRo <eduardstancu@yahoo.com>
2024-05-17 02:18:56 +0300

    install things 1

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import App from '../App.vue';
+
+const routes = [
+
+    {
+        path:'/', name:"", component: App.vue
+    },
+    {path:''}
+];
+
+const router = createRouter({
+    history:createWebHashHistory(),
+    routes
+});
+
+export default router;
